4e98506b915f02621100e13b3c3308878a488cb6
iam-vasanth <vk.vasanth.r@gmail.com>
2026-01-14 11:22:36 +0530

    Testing devshell flake

---
+++ sem-2/DotNet/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Beginner nix flake for .Net development";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+  };
+
+  outputs = { self, nixpkgs }:
+  let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in
+  {
+    devShells.${system}.default = pkgs.mkShell {
+      name = ".NET-shell";
+
+      packages = with pkgs; [
+        omnisharp-roslyn
+        dotnet-sdk_9
+      ];
+      shellHook = ''
+        echo " You're in .NET dev shell now!"
+      '';
+    };
+  };
+}
